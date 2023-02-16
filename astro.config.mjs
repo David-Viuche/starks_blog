e60a9e07965f06099d2a02e86fe06dec7b9620ca
David Viuche <jviucheroja@uniminuto.edu.co>
2023-02-15 21:51:06 -0500

    add git hub actions to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
-});
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  site: 'https://david-viuche.github.io/',
+  base: '/starks_blog'
+})
