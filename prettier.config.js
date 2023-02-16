17fad597c51e28b792a052909b790489042c9c8e
David Viuche <jviucheroja@uniminuto.edu.co>
2023-02-15 21:45:28 -0500

    config init

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')]
+  plugins: [require('prettier-plugin-tailwindcss'), require('prettier-plugin-astro')],
+  semi: false,
+  singleCoute: true,
+  trailingComma: 'none',
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
 }
